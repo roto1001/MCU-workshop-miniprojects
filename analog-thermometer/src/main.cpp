591d18331e23984b12cb7f8cb30f1f0f8298cee3
roto1001 <roberttomor8@gmail.com>
2024-10-29 19:54:09 +0100

    Added exercise-solution for analog-thermometer mini project.

---
+++ analog-thermometer/src/main.cpp
@@ -0,0 +1,65 @@
+#define ServoPin D0
+#define DhtPin D2
+#define DhtType DHT11
+
+#include <Servo.h>
+#include <DHT.h>
+
+Servo myServo;
+DHT dht(DhtPin, DhtType);
+
+float temp = 0.00f;
+float lastTemp = 0.00f;
+
+/*#################################*/
+
+bool hasTemperatureChanged() {
+  if(temp != lastTemp) {
+    lastTemp = temp;
+    return true;
+  }
+  return false;
+}
+
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+int calcServoMovement() {
+  // Remove measuring failure by adding offset.
+  float calibratedTemp = temp + 5.0f;
+
+  Serial.println("(calibrated): " + String(calibratedTemp));
+  
+  // 0 degree and 180 degree are reversed in my case because of servo positioning.
+  int degreeFloat = mapFloat(calibratedTemp, 15.0f, 25.0f, 180.0f, 0.0f);
+  return (int)round(degreeFloat);
+}
+
+
+/*#################################*/
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(2);
+}
+
+
+void loop() {
+  temp = abs(dht.readTemperature());
+
+  if(!isnan(temp)) {
+
+    Serial.println("Measured a value\n(raw): " + String(temp));
+
+    if(hasTemperatureChanged()) {
+      int degree = calcServoMovement();
+      myServo.write(degree);
+    }
+  }
+
+  // Give DHT11 sensor some time to rest.
+  delay(1000);
+}
